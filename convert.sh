4ec46219cc548e9576fa0b12e82362df2885fcc8
dmpe <cincenko@outlook.com>
2021-08-14 14:47:19 +0200

    adjust convert and butane scripts

--- convert.sh
+++ convert.sh
@@ -16,7 +16,7 @@ if [[ ! $(ls -A ./ova) ]]; then
 
 elif [[ $(find ./ova -type f -mtime +10 -print) ]]; then
     echo "File $filename exists and is older than 100 days. Removing"
-    find ./ova -type f -mtime +10 -name '*.ova' -execdir rm -- '{}' \;   
+    find ./ova -type f -mtime +10 -name '*.ova*' -execdir rm -- '{}' \;   
     docker run -v $(pwd):/work --rm --pull=always quay.io/coreos/coreos-installer:release download -p vmware -f ova -s $coreos_stream -C /work/ova/
 fi
 
